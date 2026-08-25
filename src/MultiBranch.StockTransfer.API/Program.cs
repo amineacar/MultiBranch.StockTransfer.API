490035915797d0a2b2d502603dffaf89d7e4ff82
amineacar <amineacaar@gmail.com>
2026-08-25 16:22:39 +0300

    feat: configure API startup and service registration

--- src/MultiBranch.StockTransfer.API/Program.cs
+++ src/MultiBranch.StockTransfer.API/Program.cs
@@ -1,8 +1,14 @@
 using MultiBranch.StockTransfer.Infrastructure;
+using FluentValidation;
+using MultiBranch.StockTransfer.Application.DTOs.Transfer;
+using MultiBranch.StockTransfer.Application.Validators.Transfer;
+using MultiBranch.StockTransfer.Endpoints;
+using MultiBranch.StockTransfer.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<IValidator<CreateTransferDto>, CreateTransferValidator>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,6 +22,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapCategoryEndpoints();
+app.MapProductEndpoints();
+app.MapSupplierEndpoints ();
+app.MapStoreEndpoints();
+app.MapEmployeeEndpoints();
+app.MapShelfEndpoints();
+app.MapShelfStockEndpoints();
+app.MapTransferEndpoints();
 app.Run();
